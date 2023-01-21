c925a48ddc11ec759298fb5beb926aba83a90339
Mountain <blckclst@gmail.com>
2023-01-21 21:36:15 +0500

    Add proper streams initialization

--- Assets/Scripts/CellController.cs
+++ Assets/Scripts/CellController.cs
@@ -6,11 +6,29 @@ using UniRx;
 public class CellController : MonoBehaviour
 {
     public IObservable<Vector3> mousePositionStream;
-    public IObservable<Unit> splitKeypressStream;
-    public IObservable<Unit> ejectKeypressStream;
+    public IObservable<bool> splitKeypressStream;
+    public IObservable<bool> ejectKeypressStream;
 
     public void Awake()
     {
-        // mousePositionStream =
+        mousePositionStream =
+            this.UpdateAsObservable()
+            .Select(_=> Input.mousePosition);
+
+
+        KeyCode splitKeyCode = KeyCode.Space;
+
+        splitKeypressStream =
+            this.UpdateAsObservable()
+            .Select(_=> Input.GetKeyDown(splitKeyCode))
+            .Where(b => b == true);
+
+
+        KeyCode ejectKeyCode = KeyCode.E;
+
+        ejectKeypressStream =
+            this.UpdateAsObservable()
+            .Select(_=> Input.GetKeyDown(ejectKeyCode))
+            .Where(b => b == true);
     }
 }
